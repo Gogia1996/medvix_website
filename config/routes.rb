fa630306afa6cc7a206b5605f2a379af804b9798
shweta <shwetagogia60@gmail.com>
2026-08-06 11:21:38 +0530

    Implement admin authentication, dashboard and product management

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admins
   root "pages#home"
 
   resources :products, only: [:show, :index]
   resources :inquiries, only: [:create]
+
+  namespace :admin do
+    get "dashboard/index"
+    root "dashboard#index"
+
+    resources :products
+    resources :inquiries, only: [:index, :show, :destroy]
+  end
 end
